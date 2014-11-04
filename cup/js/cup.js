2e10c3b4db281b770d75827acb057cc82674a13f
jadefeng <j.feng.1991@gmail.com>
2014-11-04 11:25:36 +1100

    window reloader

--- cup/js/cup.js
+++ cup/js/cup.js
@@ -1,73 +1,165 @@
 var cup = {
-	field: ["Admire Rakti", "Fawkner", "Red Cadeaux", "Protectionist", "Sea Moon", "Seismos", "Junoob", "Royal Diamond", "Gatewood", "Mutual Regard", "Who Shot Thebarman", "Willing Foe", "My Ambivalent", "Precedence", "Brambles", "Mr Oceirin", "Au Revoir", "Lidari", "Opinion", "Araldo", "Lucia Valentina", "Unchain My Heart", "Signoff"],
-	
-	field2: null,
-	field5: null,
+  field: ["Admire Rakti", "Fawkner", "Red Cadeaux", "Protectionist", "Seismos", "Junoob", "Royal Diamond", "Gatewood", "Mutual Regard", "Who Shot Thebarman", "Willing Foe", "My Ambivalent", "Precedence", "Brambles", "Mr Oceirin", "Au Revoir", "Lidari", "Opinion", "Araldo", "Lucia Valentina", "Unchain My Heart", "Signoff"],
 
-	sweep2: {},
-	sweep5: {},
+  field2: null,
+  field5: null,
 
-	init: function() {
-		this.field2 = _(this.field).shuffle(); 
-		this.field5 = _(this.field).shuffle(); 
+  sweep2: {},
+  sweep5: {},
 
-		cup.showField();
-		cup.showSweepsRemaining();
-	},
+  init: function () {
+    cup.reload();
 
-	showSweepsRemaining: function() {
-		var stakes2 = this.field2.length;
-		var stakes5 = this.field5.length;
+    // Create new copies of the field for each of our sweeps.
+    this.field2 = _(this.field).shuffle();
+    this.field5 = _(this.field).shuffle();
 
-		$('#stakes2').text(stakes2);
-		$('#stakes5').text(stakes5);		
-	},
+    cup.reload();
 
-	showField: function() {
-		_(this.field).each(function (horse) {
-			var $li = $('<li/>')
-			$li.text(horse);
-			$('.field ul').append($li);
-		});
-	},
+    cup.showField();
+    cup.showSweepsRemaining();
+    cup.showPrizes();
+  },
 
-	placeBet: function() {
-		var $this = $(this);
-		var gambler = $.trim(('#gambler').val());
-		$('#gambler').val('');
-		if (! gambler) { // do not execute if there is no gambler
-			return;
-		}
-		if ($this.is('#bet2')) {
-			cup.buyStake2(gambler);
-		} else {
-			cup.buyStake5(gambler);
-		}
-	},
-	buyStake2: function(gambler) {
-		// console.log("Buying a $2 stake for ", gambler);
-		var horse = this.field2.pop();
-		if(! horse) {
-			alert("Sold out");
-			return;
-		}
+  save: function() {
+  	var cupString = JSON.stringify(cup);
+  	localStorage.setItem('cup', cupString);
+  	console.log("saved");
 
-		this.sweep2[horse] = gambler;
-		$('#purchase').text([gambler], "bought", horse].join(' '));
-		this.showSweepsRemaining();
-	},
+  },
 
-	buyStake5: function(gambler) {
-		console.log("Buying a $5 stake for ", gambler);
-	},
+  reload: function() {
+  	var cupString = localStorage.getItem('cup', cupString);
+  	if (! cupString) {
+  		return; // Nothing found in local storage
+  	}
+
+  	// Merge together with history
+  	var cupCopy = JSON.parse(cupString);
+  	cup = _.extend(cup, cupCopy);
+  },
+
+  reset: function() {
+  	localStorage.removeItem('cup');
+  },
+
+  showSweepsBought: function() {
+  	var $sweep2 = $('.sweep2 ul').empty();
+  	_(this.sweep2).each(function (gambler, horse) {
+  		var $li = $('<li/>');
+  		$li.text([gambler, ': ', horse].join(" "));
+  		$sweep2.append($li);
+  	});
+
+  	var $sweep5 = $('.sweep5 ul').empty();
+  	_(this.sweep5).each(function (gambler, horse) {
+  		var $li = $('<li/>');
+  		$li.text([gambler, ': ', horse].join(" "));
+  		$sweep5.append($li);
+  	});
+
+  },
+
+  showSweepsRemaining: function () {
+    var stakes2 = this.field2.length;
+    var stakes5 = this.field5.length;
+
+    $('#stakes2').text(stakes2);
+    $('#stakes5').text(stakes5);
+  },
+
+  showField: function () {
+    _(this.field).each(function (horse) {
+      var $li = $('<li/>');
+      $li.text(horse);
+      $('.field ul').append($li);
+    });
+  },
+
+  showPrizes: function() {
+  	var currency = function (amount) {
+  		return '$' + amount.toFixed(2);
+  	};
+  	var pool2 = this.field.length * 2;
+  	var pool5 = this.field.length * 5;
+
+  	var first2 = Math.ceil(0.5 * pool2);
+  	var second2 = Math.ceil(0.3 * pool2);
+  	var wooden2 = 2;
+  	var third2 = pool2 - first2 - second2 - wooden2;
+  	$('#prize2first').text(currency( first2 ));
+ 	$('#prize2second').text(currency( second2 ));
+  	$('#prize2third').text(currency( third2 ));
+  	$('#prize2wooden').text(currency( wooden2 ));
+  	 	
+
+  	var first5 = Math.ceil(0.5 * pool5);
+  	var second5 = Math.ceil(0.3 * pool5);
+  	var wooden5 = 5;
+  	var third5 = pool5 - first5 - second5 - wooden5;
+  	$('#prize5first').text(currency( first5 ));
+ 	$('#prize5second').text(currency( second5 ));
+  	$('#prize5third').text(currency( third5 ));
+  	$('#prize5wooden').text(currency( wooden5 ));
+  	 	
+  },
+
+  highlightHorse: function(horse) {
+  	$('.active').removeClass('active');
+  	$('.field ul').find('li:contains(' + horse + ')').addClass('active');			 //sudo selector
+
+  },
+
+  placeBet: function () {
+    var $this = $(this);
+    var gambler = $.trim($('#gambler').val());
+
+    $('#gambler').val('').focus(); // Clear the input.
+
+    if (! gambler) { // You must enter a name to buy a stake.
+      return;
+    }
+
+    if ($this.is('#bet2')) {
+      cup.buyStake2(gambler);
+    } else {
+      cup.buyStake5(gambler);
+    }
+
+    cup.save();
+    cup.showSweepsBought();
+  },
+
+  buyStake2: function (gambler) {
+    var horse = this.field2.pop();
+    if (! horse) {
+      alert('Sold out');
+      return;
+    }
+
+    this.sweep2[horse] = gambler;
+    $('#purchase').text([gambler, 'bought', horse].join(' '));
+   	this.highlightHorse(horse);
+    this.showSweepsRemaining();
+  },
+
+  buyStake5: function (gambler) {
+    var horse = this.field5.pop();
+    if (! horse) {
+      alert('Sold out');
+      return;
+    }
+
+    this.sweep5[horse] = gambler;
+    $('#purchase').text([gambler, 'bought', horse].join(' '));
+   	this.highlightHorse(horse);
+    this.showSweepsRemaining();  	
+  },
 };
 
-// To get the horses names
-// http://www.racenet.com.au/melbourne-cup/field-formguide.aspx
-// $('.odds:first p').map(function (i, m) { $(m).find('span').remove(); return $(m).text().replace(/\d+. /, '') }).get() 
 
 $(document).ready(function () {
-	cup.init();
+  cup.init();
 
-	$('#bet2', '#bet5').on('click', cup.placeBet);
+  $('#bet2, #bet5').on('click', cup.placeBet);
 });
